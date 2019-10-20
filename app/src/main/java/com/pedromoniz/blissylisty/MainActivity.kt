9af2f29759d58ba0111e5f0bbf0fdd07dde18671
Pedro Moniz <pedromoniz22@gmail.com>
2019-10-20 22:47:54 +0100

    feature/FREQ-05_No-Connectivity-Screen
    Implemented the connectivity monitor. It will show a snackbar.

--- app/src/main/java/com/pedromoniz/blissylisty/MainActivity.kt
+++ app/src/main/java/com/pedromoniz/blissylisty/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.pedromoniz.blissylisty
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.ConnectivityManager
-import android.net.Network
-import androidx.appcompat.app.AppCompatActivity
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,15 +19,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        connectivityManager.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                //take action when network connection is gained
-            }
+        val filters = IntentFilter()
+        filters.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION)
+        filters.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION)
+        registerReceiver(broadcastReceiver, filters)
+    }
 
-            override fun onLost(network: Network?) {
-                //take action when network connection is lost
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager =
+            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nw = connectivityManager.activeNetwork ?: return false
+        val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+        return when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            //for other device how are able to connect with Ethernet
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+
+    private val broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent?) {
+            if (intent == null || intent.action == null)
+                return
+            when (intent.action) {
+                WifiManager.NETWORK_STATE_CHANGED_ACTION, WifiManager.WIFI_STATE_CHANGED_ACTION -> if (isNetworkAvailable()) {
+                    Snackbar.make(activityLayout, "Available Internet", Snackbar.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Snackbar.make(activityLayout, "No Internet", Snackbar.LENGTH_INDEFINITE)
+                        .show()
+
+                }
             }
-        })
+        }
     }
 }
